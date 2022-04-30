6d6055a11a3389dcbad1e9ff2bc080977ed2e095
Alex-Toucan <99103316+Alex-Toucan@users.noreply.github.com>
2022-04-30 14:39:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ require('dotenv-flow').config();
 
 const client = new Discord.Client();
 
+var prefix = !!
+
 const aboutembed = {
   title: "About",
-  description: "Ping For Toast. You **Ping**... I **DM** toast. I am possibly the most useless discord bot... but who the fuck ever wanted a useful discord bot? psssh. Amateurs. ",
+  description: "This is a Food Bot made by AT Products.",
   color: 0xc49016,
   footer: {
-    icon_url: 'https://cdn.discordapp.com/app-assets/743109027831218176/743114454115418222.png',
-    text: "This message was approved and paid for by the Ping For Toast administration lol"
+    icon_url: 'https://raw.githubusercontent.com/Alex-Toucan/Food-bot/master/files/ProfilePictureMaker.png',
+    text: "AT Products LLC (2019-2022)"
   },
   thumbnail: {
-    url: 'https://cdn.discordapp.com/app-icons/743109027831218176/3806aab678834c0e8bd3daf037b7364a.png'
+    url: 'https://raw.githubusercontent.com/Alex-Toucan/Food-bot/4b95b8e835d71a58e64a88f0a6b77babd4a1f021/files/avatar.jpeg'
   },
   author: {
     name: "By Ping For Toast (the dev... NOT the bot lol)",
@@ -23,19 +25,19 @@ const aboutembed = {
   fields: [
     {
       name: "Invite me!",
-      value: "invite me using https://discord.com/oauth2/authorize?client_id=743109027831218176&scope=bot&permissions=67488832"
+      value: "invite me using https://discord.com/oauth2/authorize?client_id=968854386363211796&scope=bot&permissions=67488832"
     },
     {
       name: "complaints?",
-      value: "DM **Ping for toast#7910**... OR make a issue at the link below!"
+      value: "DM **AlexToucanYT#0001**... or make a issue at the link below!"
     },
     {
       name: "The bot is open source!",
-      value: "Contribute or make a issue at https://github.com/Scaledi/useless-toast-bot"
+      value: "Contribute or make a issue at https://github.com/Alex-Toucan/useless-toast-bot"
     },
     {
       name: "Commands",
-      value: "Summon me in dms or any server I am on using `Toast`, `!!Toast`, or by pinging me. get this message again by typing `!!about`"
+      value: "https://raw.githubusercontent.com/Alex-Toucan/Food-bot/master/files/commands.txt"
     }
   ]
 };
@@ -46,18 +48,18 @@ const config = {
 
 client.on('ready', () => {
  console.log(`Logged in as ${client.user.tag}!`);
- client.user.setActivity('you ask for toast', {type: "WATCHING"})
+ client.user.setActivity('people ask for food.', {type: "WATCHING"})
  });
 
 
 client.on('message', msg => {
-  if(msg.content.toLowerCase() === '!!about' || msg.content.toLowerCase() === 'about the toast' || msg.content.toLowerCase() === 'toast about') {
+  if(msg.content.toLowerCase() === '${prefix}help') {
     msg.reply({ embed: aboutembed });
     }
  });
 
 client.on('message', msg => {
-  if (msg.content === 'admincontrol') {
+  if (msg.content === '${prefix}admincontrol') {
     console.log(client.guilds.cache);
     msg.reply('hi! if you are a importaint person... you can see a list of all the servers the bot is in by visiting the command prompt!');
   }
